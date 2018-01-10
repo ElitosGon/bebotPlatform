f5107c134a2da621b3df60bd6b845d5eaf1b1e26
ElitosGon <elias.gonzalezma@gmail.com>
2018-01-10 13:25:05 -0300

    HU-B1-B2-B3 register, admin register, reCaptcha Google

--- webPlatform/forms/register_forms.py
+++ webPlatform/forms/register_forms.py
@@ -2,11 +2,22 @@ from django import forms
 from webPlatform import models
 from django.contrib import auth
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'input id':'sender-email', 'placeholder':'Nombre usuario'}))
+    password1 = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Contraseña'}))
+    password2 = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Corfirmar Contraseña'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Correo'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 7, 'class':'form-control', 'placeholder':'Descripción'}))
+    avatar = forms.ImageField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'email', 'description', 'avatar')
 
 class LoginForm(forms.Form):
-	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'input id':'sender-email', 'placeholder':'Nombre usuario'}))
-	password = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Contraseña'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'input id':'sender-email', 'placeholder':'Nombre usuario'}))
+    password = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Contraseña'}))
 
-class RecoverForm(forms.Form):
-	new_password1 = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Nueva contraseña'}))
-	new_password2 = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Confirmar contraseña'}))
--- webPlatform/models.py
+++ webPlatform/models.py
@@ -1,3 +1,39 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+import os, sys
 from django.db import models
+from django.contrib import auth
+from django_resized import ResizedImageField
+from django.utils import timezone
+from datetime import datetime, timedelta
+from django.db.models import Q
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Profile(models.Model):
+	# SIMPLE FIELD 
+	description = models.CharField(default="", max_length=600, verbose_name="Descripción", null=True, blank=True)
+	avatar = ResizedImageField(size=[200,200], null=True, verbose_name="Foto perfil", blank=True, upload_to='documents/avatar/%Y/%m/%d')
+
+	# RELATION FIELD
+	user = models.OneToOneField(auth.models.User, related_name="profile", blank=True, null=True, on_delete=models.CASCADE)
+
+	# TIMESTAMP FIELD
+	created_at = models.DateTimeField(auto_now_add=True, blank=True, verbose_name="Fecha creación")
+	updated_at = models.DateTimeField(auto_now=True, blank=True, verbose_name="Fecha última modificación")
+
+	def __str__(self):
+		return "%d" %self.id
+
+@receiver(post_save, sender=auth.models.User)
+def update_user_profile(sender, instance, created, **kwargs):
+    try:
+    	if created:
+        	Profile.objects.create(user=instance)
+    	instance.profile.save()
+    except:	
+    	return instance
