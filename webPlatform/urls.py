9f62aaa648c423396cca6920fb0ea2966cc635b1
ElitosGon <elias.gonzalezma@gmail.com>
2018-01-10 22:06:18 -0300

    Delete profile

--- webPlatform/urls.py
+++ webPlatform/urls.py
@@ -7,11 +7,13 @@ from django.contrib.auth.views import password_reset, password_reset_confirm, pa
 urlpatterns = [
 	# General
 	url(r'^$', general_views.home, name= "home"), 
-	
+	url(r'^collaborators/$', general_views.collaborators, name= "collaborators"), 
+
 	# Collaborator
 	url(r'^account/$', collaborator_views.my_account, name= "my_account"), 
 	url(r'^account/update/$', collaborator_views.my_account_update, name= "my_account_update"),
 	url(r'^account/change/password/$', collaborator_views.my_account_change_password, name= "my_account_change_password"), 
+	url(r'^account/delete/$', collaborator_views.my_account_delete, name= "my_account_delete"), 
 
 
 
@@ -20,7 +22,4 @@ urlpatterns = [
     url(r'^password/reset/complete/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html' }, name='password_reset_complete'),
 	url(r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
 	
-
-
-
 ]
--- webPlatform/views/general_views.py
+++ webPlatform/views/general_views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from datetime import datetime
 from django.db.models import Q
 from django.conf import settings
+from django.http import Http404
 
 ####### HOME #####################################
 def home(request):
@@ -14,3 +15,19 @@ def home(request):
 	else:
 		return render(request,'error/404.html', None, RequestContext(request))
 
+####### Collaborators ############################
+def collaborators(request):
+	if request.method == 'GET':
+		collaborators = models.Profile.objects.all().order_by('updated_at')
+		query = request.GET.get("search")
+		
+		if query:
+			collaborators = collaborators.filter(Q(user__first_name__icontains=query) & Q(user__last_name__icontains=query)).distinct()
+		
+		contexto = {
+			'collaborators' : collaborators
+		}
+
+		return render(request,'general/collaborators.html', contexto ,RequestContext(request))
+	else:
+		return Http404
